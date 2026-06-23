d94e261a70b72b33e1719213400e552c4e3e3a4b
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-23 16:22:33 -0600

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,118 @@
+import { auth, db } from "./firebase-config.js";
+
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+const btnLogin =
+document.getElementById("btnLogin");
+
+
+btnLogin.addEventListener(
+    "click",
+    iniciarSesion
+);
+
+
+async function iniciarSesion(){
+
+    const telefono =
+    document.getElementById("telefono").value.trim();
+
+    const password =
+    document.getElementById("password").value;
+
+    if(!telefono || !password){
+
+        alert(
+            "Completa todos los campos."
+        );
+
+        return;
+    }
+
+    try{
+
+        const email =
+        `${telefono}@moti.app`;
+
+        const cred =
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        const uid =
+        cred.user.uid;
+
+        const docRef =
+        doc(db, "usuarios", uid);
+
+        const docSnap =
+        await getDoc(docRef);
+
+        if(!docSnap.exists()){
+
+            alert(
+                "Usuario no encontrado."
+            );
+
+            return;
+        }
+
+        const usuario =
+        docSnap.data();
+
+        console.log(usuario);
+
+        if(usuario.passwordTemporal){
+
+            window.location.href =
+            "cambiar-password.html";
+
+            return;
+        }
+
+        if(usuario.tipo === "pasajero"){
+
+            window.location.href =
+            "dashboard-pasajero.html";
+
+            return;
+        }
+
+        if(usuario.tipo === "conductor"){
+
+            if(usuario.estado === "pendiente"){
+
+                window.location.href =
+                "conductor-pendiente.html";
+
+            }else{
+
+                window.location.href =
+                "dashboard-conductor.html";
+
+            }
+
+        }
+
+    }
+    catch(error){
+
+        console.error(error);
+
+        alert(
+            "Teléfono o contraseña incorrectos."
+        );
+
+    }
+
+}
