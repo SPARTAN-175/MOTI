5f79f4cbffd6b68031e3a1e25d3dd88fa771b08c
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-23 10:11:50 -0600

    Update firebase-test.js

--- js/firebase-test.js
+++ js/firebase-test.js
@@ -1,5 +1,48 @@
 import { db } from "./firebase-config.js";
 
-console.log("🔥 Firebase conectado correctamente");
+import {
 
-console.log(db);
+    doc,
+
+    getDoc
+
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+async function leerUsuario() {
+
+    try {
+
+        const docRef =
+        doc(db, "usuarios", "usuario_prueba");
+
+        const docSnap =
+        await getDoc(docRef);
+
+        if (docSnap.exists()) {
+
+            console.log(
+                "🔥 Usuario encontrado:"
+            );
+
+            console.log(
+                docSnap.data()
+            );
+
+        } else {
+
+            console.log(
+                "❌ Usuario no encontrado"
+            );
+
+        }
+
+    } catch(error) {
+
+        console.error(error);
+
+    }
+
+}
+
+leerUsuario();
