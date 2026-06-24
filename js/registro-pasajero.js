3daee8ff1b9f1d16ddb669d61d6b2b370e7a36d0
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-24 09:44:50 -0600

    Update registro-pasajero.js

--- js/registro-pasajero.js
+++ js/registro-pasajero.js
@@ -123,11 +123,24 @@ async function registrarUsuario() {
     }
     catch(error){
 
-        console.error(error);
+    console.error(error);
+
+    if(
+        error.code ===
+        "auth/email-already-in-use"
+    ){
 
         alert(
-            error.message
+            "Ya existe una cuenta registrada con este número telefónico."
         );
+
+        return;
+
+    }
+
+    alert(
+        "No se pudo crear la cuenta."
+    );
 
     }
 
