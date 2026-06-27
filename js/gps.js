7f4e8443f15ab2461f83fe6f89ca9d471ce2bf3d
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-27 12:44:57 -0600

    Create gps.js

---
+++ js/gps.js
@@ -0,0 +1,100 @@
+import { auth, db }
+from "./firebase-config.js";
+
+import {
+    onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    doc,
+    updateDoc
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+onAuthStateChanged(
+
+    auth,
+
+    (user)=>{
+
+        if(!user) return;
+
+        iniciarGPS(user.uid);
+
+    }
+
+);
+
+
+function iniciarGPS(uid){
+
+    if(!("geolocation" in navigator)){
+
+        console.log(
+            "GPS no compatible"
+        );
+
+        return;
+
+    }
+
+    navigator.geolocation.watchPosition(
+
+        async(position)=>{
+
+            const lat =
+            position.coords.latitude;
+
+            const lng =
+            position.coords.longitude;
+
+            await updateDoc(
+
+                doc(
+                    db,
+                    "usuarios",
+                    uid
+                ),
+
+                {
+
+                    latitud:lat,
+
+                    longitud:lng,
+
+                    ultimaUbicacion:
+                    new Date().toISOString()
+
+                }
+
+            );
+
+            console.log(
+                "📍 GPS actualizado",
+                lat,
+                lng
+            );
+
+        },
+
+        (error)=>{
+
+            console.error(error);
+
+        },
+
+        {
+
+            enableHighAccuracy:true,
+
+            maximumAge:5000,
+
+            timeout:10000
+
+        }
+
+    );
+
+}
