96265d927b175fec8b12f190bf1c40b39093e2af
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-29 10:47:57 -0600

    Update motor-asignacion.js

--- js/motor/motor-asignacion.js
+++ js/motor/motor-asignacion.js
@@ -1,1 +1,45 @@
+import { buscarConductores }
 
+from "./buscar-conductores.js";
+
+const conductores = [
+
+    {
+
+        nombre:"Carlos",
+
+        estadoServicio:"disponible"
+
+    },
+
+    {
+
+        nombre:"Juan",
+
+        estadoServicio:"en_viaje"
+
+    },
+
+    {
+
+        nombre:"Pedro",
+
+        estadoServicio:"disponible"
+
+    },
+
+    {
+
+        nombre:"Luis",
+
+        estadoServicio:"ocupado"
+
+    }
+
+];
+
+const disponibles =
+
+buscarConductores(conductores);
+
+console.log(disponibles);
