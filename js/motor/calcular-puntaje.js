26538fa6d17fe634af138a89ae184cb9b1aacb1b
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-29 11:39:05 -0600

    Update calcular-puntaje.js

--- js/motor/calcular-puntaje.js
+++ js/motor/calcular-puntaje.js
@@ -1,1 +1,79 @@
+export function calcularPuntaje(conductores){
 
+    conductores.forEach(
+
+        conductor=>{
+
+            let puntos = 0;
+
+            // =====================
+            // DISTANCIA
+            // =====================
+
+            if(conductor.distancia<=300){
+
+                puntos +=100;
+
+            }
+
+            else if(conductor.distancia<=500){
+
+                puntos +=80;
+
+            }
+
+            else if(conductor.distancia<=1000){
+
+                puntos +=60;
+
+            }
+
+            else if(conductor.distancia<=2000){
+
+                puntos +=40;
+
+            }
+
+            else{
+
+                puntos +=20;
+
+            }
+
+            // =====================
+            // ROTACIÓN
+            // =====================
+
+            if(conductor.minutosSinViaje<=10){
+
+                puntos +=10;
+
+            }
+
+            else if(conductor.minutosSinViaje<=20){
+
+                puntos +=20;
+
+            }
+
+            else if(conductor.minutosSinViaje<=40){
+
+                puntos +=40;
+
+            }
+
+            else{
+
+                puntos +=60;
+
+            }
+
+            conductor.puntaje = puntos;
+
+        }
+
+    );
+
+    return conductores;
+
+}
