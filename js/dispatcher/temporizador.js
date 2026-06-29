87e9ee705c3d06f06fe0d940d2940604f19844d9
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-29 13:16:52 -0600

    Update temporizador.js

--- js/dispatcher/temporizador.js
+++ js/dispatcher/temporizador.js
@@ -1,1 +1,27 @@
+export function esperar(
 
+    segundos
+
+){
+
+    return new Promise(
+
+        resolve=>{
+
+            setTimeout(
+
+                ()=>{
+
+                    resolve();
+
+                },
+
+                segundos*1000
+
+            );
+
+        }
+
+    );
+
+}
