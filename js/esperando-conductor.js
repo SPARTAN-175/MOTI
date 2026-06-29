8491c5cdcbb6a599bafa09d4de533abf6d6d935d
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-29 15:54:43 -0600

    Update esperando-conductor.js

--- js/esperando-conductor.js
+++ js/esperando-conductor.js
@@ -1,1 +1,86 @@
+import { db }
+from "./firebase-config.js";
 
+import {
+
+doc,
+
+onSnapshot
+
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+const params =
+
+new URLSearchParams(
+
+window.location.search
+
+);
+
+const solicitudId =
+
+params.get("id");
+
+
+if(!solicitudId){
+
+    alert("Solicitud no encontrada.");
+
+    window.location.href =
+    "dashboard-pasajero.html";
+
+}
+
+
+const solicitudRef =
+
+doc(
+
+db,
+
+"solicitudes",
+
+solicitudId
+
+);
+
+
+onSnapshot(
+
+solicitudRef,
+
+(docSnap)=>{
+
+    if(!docSnap.exists()) return;
+
+    const solicitud =
+
+    docSnap.data();
+
+    console.log(
+
+        "Estado:",
+
+        solicitud.estado
+
+    );
+
+    if(
+
+        solicitud.estado ===
+
+        "aceptada"
+
+    ){
+
+        window.location.href=
+
+        `viaje-activo-pasajero.html?id=${solicitudId}`;
+
+    }
+
+}
+
+);
