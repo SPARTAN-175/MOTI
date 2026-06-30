bc5d52e12a60364d53e0cc7843752ff876299792
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-29 18:40:33 -0600

    Update guardar-destino.js

--- js/destinos/guardar-destino.js
+++ js/destinos/guardar-destino.js
@@ -1,1 +1,72 @@
+import { db }
+from "../firebase-config.js";
 
+import {
+
+    collection,
+    addDoc,
+    serverTimestamp
+
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+export async function guardarDestino(destino){
+
+    try{
+
+        const docRef =
+
+        await addDoc(
+
+            collection(
+
+                db,
+
+                "destinosEspeciales"
+
+            ),
+
+            {
+
+                nombre:
+
+                destino.nombre,
+
+                latitud:
+
+                destino.latitud,
+
+                longitud:
+
+                destino.longitud,
+
+                activo:true,
+
+                fechaCreacion:
+
+                serverTimestamp()
+
+            }
+
+        );
+
+        return{
+
+            id:docRef.id,
+
+            ...destino
+
+        };
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        return null;
+
+    }
+
+}
