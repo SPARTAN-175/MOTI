a50c1ba41155ff02f812ff289221aa524c92ce19
SPARTAN-175 <avengers_0077@hotmail.com>
2026-06-30 09:50:34 -0600

    Update historial-conductor.js

--- js/historial-conductor.js
+++ js/historial-conductor.js
@@ -1,1 +1,111 @@
+import { auth, db }
+from "./firebase-config.js";
 
+import {
+
+collection,
+query,
+where,
+getDocs,
+doc,
+getDoc
+
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+
+onAuthStateChanged
+
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+const lista =
+
+document.getElementById(
+
+"listaHistorial"
+
+);
+
+
+onAuthStateChanged(
+
+auth,
+
+async(user)=>{
+
+if(!user)return;
+
+await cargarHistorial(
+
+user.uid
+
+);
+
+});
+
+
+
+async function cargarHistorial(uid){
+
+lista.innerHTML="";
+
+const consulta=
+
+query(
+
+collection(
+
+db,
+
+"solicitudes"
+
+),
+
+where(
+
+"conductorId",
+
+"==",
+
+uid
+
+)
+
+);
+
+const snapshot=
+
+await getDocs(
+
+consulta
+
+);
+
+if(snapshot.empty){
+
+lista.innerHTML=
+
+"<p>No tienes viajes registrados.</p>";
+
+return;
+
+}
+
+for(const viajeDoc of snapshot.docs){
+
+const viaje=
+
+viajeDoc.data();
+
+crearTarjeta(
+
+viaje
+
+);
+
+}
+
+}
