ae7aafa9f593dc3321f8fcaa3ebbe003789257b2
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-01 10:55:19 -0600

    Update elegir-conductor.js

--- js/elegir-conductor.js
+++ js/elegir-conductor.js
@@ -1,1 +1,49 @@
+import { auth, db }
+from "./firebase-config.js";
 
+import {
+
+collection,
+query,
+where,
+onSnapshot
+
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+const params =
+
+new URLSearchParams(
+
+window.location.search
+
+);
+
+const destinoId =
+
+params.get(
+
+"destinoId"
+
+);
+
+const lista =
+
+document.getElementById(
+
+"listaConductores"
+
+);
+
+const nombreDestino =
+
+document.getElementById(
+
+"nombreDestino"
+
+);
+
+nombreDestino.textContent =
+
+destinoId;
