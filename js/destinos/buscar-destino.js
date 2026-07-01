45fcef612f029e944a430afb37137977879ad088
SPARTAN-175 <avengers_0077@hotmail.com>
2026-07-01 11:19:40 -0600

    Update buscar-destino.js

--- js/destinos/buscar-destino.js
+++ js/destinos/buscar-destino.js
@@ -5,34 +5,14 @@ import {
 }
 from "./cargar-destinos.js";
 
-import {
-
-    buscarEnNominatim
-
-}
-from "./nominatim.js";
-
-import {
-
-    guardarDestino
-
-}
-from "./guardar-destino.js";
-
 
 export async function buscarDestino(texto){
-
-    // ===========================
-    // BUSCAR EN FIREBASE
-    // ===========================
 
     const destinos =
 
     await cargarDestinos();
 
-    const encontrados =
-
-    destinos.filter(
+    return destinos.filter(
 
         destino=>
 
@@ -47,59 +27,5 @@ export async function buscarDestino(texto){
         )
 
     );
-
-    if(
-
-        encontrados.length>0
-
-    ){
-
-        console.log(
-
-            "Destino encontrado en Firebase"
-
-        );
-
-        return encontrados;
-
-    }
-
-    // ===========================
-    // BUSCAR EN INTERNET
-    // ===========================
-
-    console.log(
-
-        "Buscando en OpenStreetMap..."
-
-    );
-
-    const internet =
-
-    await buscarEnNominatim(
-
-        texto
-
-    );
-
-    // ===========================
-    // GUARDAR EL PRIMERO
-    // ===========================
-
-    if(
-
-        internet.length>0
-
-    ){
-
-        await guardarDestino(
-
-            internet[0]
-
-        );
-
-    }
-
-    return internet;
 
 }
